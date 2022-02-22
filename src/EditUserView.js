d1bdb31568459444bd860c2f60758b9e2600c6c6
Rafal Welzant <welzant@gmail.com>
2022-02-22 21:02:07 +0100

    dashboard_checking

--- src/EditUserView.js
+++ src/EditUserView.js
@@ -1,11 +1,10 @@
-import { useParams, useNavigate, Navigate } from 'react-router-dom'
+import { useParams, Navigate } from 'react-router-dom'
 import { useUsers } from './contexts/UsersContext';
 import { EditUserForm } from './EditUserForm';
 
 export const EditUserView = () => {
   const { getUserById } = useUsers();
   const { userId } = useParams();
-  const navigate = useNavigate();
   const user = getUserById(Number(userId));
 
   if (!user) {
